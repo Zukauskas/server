e64be21843e88146f33d63e74e281e829c88131e
Tautvydas Žukauskas <118177903+Zukauskas@users.noreply.github.com>
2023-01-19 13:31:11 +0200

    Initial code

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-console.log("serveris...")
+import { server } from "./lib/server.js";
+
+const app = {};
+
+
+//paleidinejam projekta
+app.init = () => {
+    // trukstamo pradinio turinio generavimas
+    // -- folderiai
+    // -- failai
+    // gauti prisijungima prie DB
+    // uzkurti serveri
+    server.init()
+
+    // const timer = setInterval(() => {
+    //     console.log("apsivalymas....")
+
+    //     // reguliarus procesai
+    //     // - info sinchronizavimas ir atsinaujinimas
+    //     // - failu archyvavimas
+    //     // - info agregavimas (statistika)
+    //     // - nereikalingos info salinimas:
+    //     //      - failu trynimas
+    //     //      - DB optimizavimas 
+    // }, 5000)
+}
+
+app.init();
+
+export { app }
