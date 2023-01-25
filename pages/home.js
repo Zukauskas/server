3edb13907943d4c5d8a3a350e96e60ea5fe4ba72
Tautvydas Žukauskas <118177903+Zukauskas@users.noreply.github.com>
2023-01-25 17:35:13 +0200

    Update

--- pages/home.js
+++ pages/home.js
@@ -2,7 +2,8 @@ import { PageTemplate } from "../lib/PageTemplate.js";
 
 class PageHome extends PageTemplate {
     mainHTML() {
-        return `<h1>Welcome to Home Page</h1>`;
+        return `<h1>Welcome to HOME PAGE!</h1>
+                <img src="/images/logo-dark.png" alt="neuron">`;
     }
 }
 
